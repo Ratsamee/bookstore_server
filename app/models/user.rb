f01a6eff46767fa9145d2d0b6a927d29be8efd13
Ratsamee <jayin@hotmail.com>
2019-09-12 15:05:16 +1000

    fixed http error code 500

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@
 
 class User < ApplicationRecord
     has_secure_password
-    # validates :email, presence: true, uniqueness: { case_sensitive: false }
-    # validates :full_name, presence: true
-    # validates :shipping_address, presence: true
-    # has_many :orders
-    # has_many :orderdetails, through: :orders
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :full_name, presence: true
+    validates :shipping_address, presence: true
+    has_many :orders
+    has_many :orderdetails, through: :orders
 end
